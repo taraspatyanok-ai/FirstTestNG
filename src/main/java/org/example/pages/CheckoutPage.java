8329c5ca54982a08213a529020f940d8e115ecd2
taras <taraspatyanok@gmail.com>
2026-05-11 03:51:06 -0700

    just new line

--- src/main/java/org/example/pages/CheckoutPage.java
+++ src/main/java/org/example/pages/CheckoutPage.java
@@ -1,5 +1,15 @@
 package org.example.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CheckoutPage {
     private WebDriver driver;
     @FindBy(xpath = "//button[@id= 'checkout']")
